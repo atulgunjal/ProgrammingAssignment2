bdb1296be169744d7b984bbda93b166a7498983a
atul.gunjal <atul.gunjal@iworktech.com>
2017-04-11 17:09:43 +0530

    Committed by Atul Gunjal on 11 April 2017

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,25 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+## This function call the inverse of matrix if it does not present in cache
+##It is mandetory to clear cache elements matrixA, inverseofmatrixA 
+##before calling the function
+
+myfunction <- function(fnelement = c(4,7,2,6), fnrow=2, fncol=2)
+{
+  
+  mymatrix <- matrix(fnelement, nrow = fnrow, ncol = fncol,byrow = TRUE)
+ 
+  cacheResult <- cacheSolve(mymatrix) ##In this step Inverse of matrix function is called
+  cacheresult1 <- cacheSolve(mymatrix)  ##In this step cache element is used
+  
+  
+}
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  matrixA <<- x   ##copy original matric into cache
+  inverseofmatrixA <<- solve(x)  ## take inverse of original matrix and copy it into the cache element
 
 }
 
@@ -11,5 +27,16 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## Return a matrix that is the inverse of 'x' by this function either invesre is calulated or 
+        ## invese for cache is returned back. 
+  if (!is.null(inverseofmatrixA) && identical(x, matrixA))
+  {
+    inverseofmatrixA
+  }
+  else
+  {
+    makeCacheMatrix(x)
+  }
+  
+  
 }
